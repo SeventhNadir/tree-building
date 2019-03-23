83f6d15e41d7c7b7ad033fc5fa00139eb6cc82c4
Josh Crane <josh.crane512@gmail.com>
2019-03-23 11:42:25 +0800

    Update tree.go

--- tree.go
+++ tree.go
@@ -22,56 +22,36 @@ func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
-	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
 
-	err := errorCheck(records)
-	if err != nil {
-		return nil, err
-	}
-
-	parentChild := getParentChildMap(records)
-	root := &Node{ID: records[0].ID}
-	root.Children = getChildren(root, parentChild)
-	return root, nil
-}
-
-func getParentChildMap(records []Record) map[int][]*Node {
-	childMap := make(map[int][]*Node)
-	for _, record := range records {
-		parentID := record.Parent
-		childNode := &Node{record.ID, nil}
-		childMap[parentID] = append(childMap[parentID], childNode)
-	}
-	return childMap
-}
-
-func getChildren(parent *Node, parentChild map[int][]*Node) []*Node {
-	var nodeArray []*Node
-	for _, child := range parentChild[parent.ID] {
-		if child.ID != parent.ID {
-			node := &Node{ID: child.ID}
-			node.Children = getChildren(node, parentChild)
-			nodeArray = append(nodeArray, node)
-		}
-	}
-	return nodeArray
-}
-
-func errorCheck(records []Record) error {
 	for i, record := range records {
 		if i != record.ID {
-			return errors.New("non-contiguous")
+			return nil, errors.New("non-contiguous")
 		}
 		if record.ID < record.Parent {
-			return errors.New("higher id parent of lower id")
+			return nil, errors.New("higher id parent of lower id")
 		}
 		originalRecord := record
 		for record.ID != 0 {
 			record = records[record.Parent]
 			if record == originalRecord {
-				return errors.New("cycle indirectly")
+				return nil, errors.New("cycle indirectly")
 			}
 		}
 	}
-	return nil
+
+	nodes := make([]*Node, len(records))
+	records = records[1:]
+
+	nodes[0] = &Node{ID: 0}
+	for _, record := range records {
+		parent := nodes[record.Parent]
+		node := &Node{ID: record.ID}
+		nodes[node.ID] = node
+		parent.Children = append(parent.Children, node)
+	}
+	return nodes[0], nil
+
 }
